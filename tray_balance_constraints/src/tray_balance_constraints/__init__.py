2d404305ff04bbac2814d3ad0a31991fff5d9f29
Adam Heins <mail@adamheins.com>
2022-03-22 18:16:03 -0400

    Add alternative max eigenvalue approximation for radii of gyration.

--- tray_balance_constraints/src/tray_balance_constraints/__init__.py
+++ tray_balance_constraints/src/tray_balance_constraints/__init__.py
@@ -7,14 +7,19 @@ from tray_balance_constraints.bindings import *
 import IPython
 
 
+# TODO centralize a single implementation
+def skew3(v):
+    x, y, z = v
+    return np.array([[0, -z, y], [z, 0, -x], [-y, x, 0]])
+
+
 def compute_com(masses, coms):
     assert masses.shape[1] == 1, "masses.shape[1] != 1"
     return np.sum(masses * coms, axis=0) / np.sum(masses)
 
 
-class BoundingEllipsoidProblem:
-    def __init__(self, ell, bodies):
-        self.ell = ell
+class BoundingOptProblem:
+    def __init__(self, bodies):
         self.bodies = bodies
         self.n_bodies = len(bodies)
 
@@ -29,17 +34,12 @@ class BoundingEllipsoidProblem:
         coms = x[self.n_bodies :].reshape((self.n_bodies, 3))
         return masses, coms
 
-    def _cost(self, x):
-        masses, coms = self._parse_args(x)
-        com = compute_com(masses, coms)
-        delta = com - self.ell.center()
-
-        # our goal is maximization so make negative
-        return -delta.T @ self.ell.E() @ delta
-
     def _ineq_constraints(self, x):
         # inequality constraints are formulated to be non-negative
-        masses, coms = self._parse_args(x)
+
+        # always want to call this class's version of _parse_args (child
+        # classes may have overridden it to add new variables)
+        masses, coms = BoundingOptProblem._parse_args(self, x)
 
         # each body's mass is within [mass_min, mass_max]
         mass_constraints = self.Am @ masses.flatten() + self.bm
@@ -56,7 +56,7 @@ class BoundingEllipsoidProblem:
     def _eq_constraints(self, x):
         # equality constraints are required whenever at least one of the CoM
         # ellipsoids of the composing bodies is not full rank
-        _, coms = self._parse_args(x)
+        _, coms = BoundingOptProblem._parse_args(self, x)
 
         constraints = []
         for i, body in enumerate(self.bodies):
@@ -66,6 +66,20 @@ class BoundingEllipsoidProblem:
                 v = V[:, -(j + 1)]
                 constraints.append(v @ (c - coms[i, :]))
         return np.array(constraints)
+
+
+class BoundingEllipsoidProblem(BoundingOptProblem):
+    def __init__(self, ell, bodies):
+        self.ell = ell
+        super().__init__(bodies)
+
+    def _cost(self, x):
+        masses, coms = self._parse_args(x)
+        com = compute_com(masses, coms)
+        delta = com - self.ell.center()
+
+        # our goal is maximization so make negative
+        return -delta.T @ self.ell.E() @ delta
 
     def solve(self, masses0, coms0, method="SLSQP"):
         """Solve the optimization problem given the initial guess."""
@@ -84,21 +98,14 @@ class BoundingEllipsoidProblem:
         return masses_opt, coms_opt
 
 
-class BoundingRadiiOfGyrationProblem:
+class BoundingRadiiOfGyrationProblem(BoundingOptProblem):
+    """Bounding radii of gyration based on diagonal approximation."""
     def __init__(self, bodies):
-        self.bodies = bodies
-        self.n_bodies = len(bodies)
-
-        # mass constraint matrices
-        self.Am = np.kron(np.eye(self.n_bodies), np.array([[1], [-1]]))
-        self.bm = np.concatenate([(-body.mass_min, body.mass_max) for body in bodies])
-
-        self.n_eq_con = np.sum([3 - body.com_ellipsoid.rank() for body in bodies])
+        super().__init__(bodies)
 
     def _parse_args(self, x):
         s = x[0]
-        masses = x[1 : self.n_bodies + 1].reshape((self.n_bodies, 1))
-        coms = x[self.n_bodies + 1 :].reshape((self.n_bodies, 3))
+        masses, coms = super()._parse_args(x[1:])
         return s, masses, coms
 
     def _cost(self, x):
@@ -122,31 +129,12 @@ class BoundingRadiiOfGyrationProblem:
 
         psd_constraint = Ajj - s
 
-        # each body's mass is within [mass_min, mass_max]
-        mass_constraints = self.Am @ masses.flatten() + self.bm
+        mass_com_constraints = super()._ineq_constraints(x[1:])
 
-        # each body's CoM is within its bounding ellipsoid
-        com_constraints = np.zeros(self.n_bodies)
-        for i in range(self.n_bodies):
-            delta = coms[i, :] - self.bodies[i].com_ellipsoid.center()
-            E = self.bodies[i].com_ellipsoid.E()
-            com_constraints[i] = 1 - delta.T @ E @ delta
-
-        return np.concatenate(([psd_constraint], mass_constraints, com_constraints))
+        return np.concatenate(([psd_constraint], mass_com_constraints))
 
     def _eq_constraints(self, x):
-        # equality constraints are required whenever at least one of the CoM
-        # ellipsoids of the composing bodies is not full rank
-        _, _, coms = self._parse_args(x)
-
-        constraints = []
-        for i, body in enumerate(self.bodies):
-            for j in range(3 - body.com_ellipsoid.rank()):
-                V = body.com_ellipsoid.directions()
-                c = body.com_ellipsoid.center()
-                v = V[:, -(j + 1)]
-                constraints.append(v @ (c - coms[i, :]))
-        return np.array(constraints)
+        return super()._eq_constraints(x[1:])
 
     def _solve_one(self, x0, j, method="SLSQP"):
         """Solve the optimization problem given the initial guess."""
@@ -171,6 +159,63 @@ class BoundingRadiiOfGyrationProblem:
             s_opt, masses_opt, coms_opt = self._solve_one(x0, j, method=method)
             radii_squared_opt[j] = s_opt
         return radii_squared_opt
+
+
+class BoundingRadiiOfGyrationProblem2(BoundingOptProblem):
+    """Alternative bounding radii implementation based on max eigenvalue."""
+    def __init__(self, bodies):
+        super().__init__(bodies)
+
+    def _parse_args(self, x):
+        s = x[0]
+        u = x[1:4]
+        masses, coms = super()._parse_args(x[4:])
+        return s, u, masses, coms
+
+    def _cost(self, x):
+        s, _, _, _ = self._parse_args(x)
+
+        # our goal is maximization so make negative
+        return -s
+
+    def _ineq_constraints(self, x):
+        # inequality constraints are formulated to be non-negative
+        s, u, masses, coms = self._parse_args(x)
+        com = compute_com(masses, coms)
+
+        A = np.zeros((3, 3))
+        for i in range(self.n_bodies):
+            P_i = skew3(com - coms[i, :])
+            R_i = np.diag(self.bodies[i].radii_of_gyration)
+            A += masses[i, 0] * (R_i @ R_i + P_i.T @ P_i)
+        A /= np.sum(masses)
+
+        ev_constraint = u.T @ A @ u - s
+        mass_com_constraints = super()._ineq_constraints(x[4:])
+        constraints = np.concatenate(([ev_constraint], mass_com_constraints))
+        return constraints
+
+    def _eq_constraints(self, x):
+        _, u, _, _ = self._parse_args(x)
+        u_norm_constraint = 1 - u @ u
+        default_cons = super()._eq_constraints(x[4:])
+        return np.concatenate(([u_norm_constraint], default_cons))
+
+    def solve(self, masses0, coms0, method="SLSQP"):
+        """Solve the optimization problem given the initial guess."""
+        x0 = np.concatenate(([0, 0, 0, 1], masses0, coms0.flatten()))
+        constraints = [{"type": "ineq", "fun": self._ineq_constraints}]
+        if self.n_eq_con > 0:
+            constraints.append({"type": "eq", "fun": self._eq_constraints})
+        res = minimize(
+            self._cost,
+            x0,
+            method=method,
+            constraints=constraints,
+        )
+        assert res.success, "Optimization problem failed to solve."
+        s_opt, u_opt, masses_opt, coms_opt = self._parse_args(res.x)
+        return s_opt * np.ones(3)
 
 
 def sample_com(bodies, boundary=True):
