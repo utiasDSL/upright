66470b4902197fdc60928b4b7a870a13cf530df7
Adam Heins <mail@adamheins.com>
2022-03-22 14:53:05 -0400

    Initial implementation of bounding radii of gyration.

--- tray_balance_constraints/scripts/ellipsoid.py
+++ tray_balance_constraints/scripts/ellipsoid.py
@@ -6,8 +6,8 @@ import tray_balance_constraints as con
 
 import IPython
 
-N = 100
 
+N = 100
 
 center = np.array([0, 0, 0])
 half_lengths = np.array([0.1, 0.1, 0.2])
@@ -21,7 +21,7 @@ half_lengths = np.array([0.1, 0.1, 0])
 directions = np.eye(3)
 ell2 = con.Ellipsoid(center, half_lengths, directions, rank=2)
 # ell2 = con.Ellipsoid.point(center)
-radii2 = 0.1 * np.ones(3)
+radii2 = 0.1 * np.array([1, 1, 2])
 
 body1 = con.BoundedRigidBody(
     mass_min=0.25, mass_max=0.5, radii_of_gyration=radii1, com_ellipsoid=ell1
@@ -33,20 +33,3 @@ body2 = con.BoundedRigidBody(
 composite = con.BoundedRigidBody.compose([body1, body2])
 
 IPython.embed()
-
-# ell = con.Ellipsoid.bounding_ellipsoid(P, 0.01)
-# R = scipy.linalg.orth(P.T)
-#
-# # project points into R basis
-# PR = P @ R
-#
-# c, hl, V = bounding_ellipsoid(PR, 0.01)
-#
-# # project the bounding ellipsoid back into the original space
-# directions = R @ V
-# center = R @ c
-# half_lengths = hl
-#
-# ell = con.Ellipsoid(center, half_lengths, directions, rank=R.shape[1])
-
-# IPython.embed()
--- tray_balance_constraints/src/tray_balance_constraints/__init__.py
+++ tray_balance_constraints/src/tray_balance_constraints/__init__.py
@@ -84,6 +84,95 @@ class BoundingEllipsoidProblem:
         return masses_opt, coms_opt
 
 
+class BoundingRadiiOfGyrationProblem:
+    def __init__(self, bodies):
+        self.bodies = bodies
+        self.n_bodies = len(bodies)
+
+        # mass constraint matrices
+        self.Am = np.kron(np.eye(self.n_bodies), np.array([[1], [-1]]))
+        self.bm = np.concatenate([(-body.mass_min, body.mass_max) for body in bodies])
+
+        self.n_eq_con = np.sum([3 - body.com_ellipsoid.rank() for body in bodies])
+
+    def _parse_args(self, x):
+        s = x[0]
+        masses = x[1 : self.n_bodies + 1].reshape((self.n_bodies, 1))
+        coms = x[self.n_bodies + 1 :].reshape((self.n_bodies, 3))
+        return s, masses, coms
+
+    def _cost(self, x):
+        s, _, _ = self._parse_args(x)
+
+        # our goal is maximization so make negative
+        return -s
+
+    def _ineq_constraints(self, x, *args):
+        # inequality constraints are formulated to be non-negative
+        j = args[0]  # index
+        s, masses, coms = self._parse_args(x)
+        com = compute_com(masses, coms)
+
+        Ajj = 0
+        for i in range(self.n_bodies):
+            p_i = com - coms[i, :]
+            r_gyr = self.bodies[i].radii_of_gyration[j]
+            Ajj += masses[i, 0] * (r_gyr ** 2 + p_i @ p_i)
+        Ajj /= np.sum(masses)
+
+        psd_constraint = Ajj - s
+
+        # each body's mass is within [mass_min, mass_max]
+        mass_constraints = self.Am @ masses.flatten() + self.bm
+
+        # each body's CoM is within its bounding ellipsoid
+        com_constraints = np.zeros(self.n_bodies)
+        for i in range(self.n_bodies):
+            delta = coms[i, :] - self.bodies[i].com_ellipsoid.center()
+            E = self.bodies[i].com_ellipsoid.E()
+            com_constraints[i] = 1 - delta.T @ E @ delta
+
+        return np.concatenate(([psd_constraint], mass_constraints, com_constraints))
+
+    def _eq_constraints(self, x):
+        # equality constraints are required whenever at least one of the CoM
+        # ellipsoids of the composing bodies is not full rank
+        _, _, coms = self._parse_args(x)
+
+        constraints = []
+        for i, body in enumerate(self.bodies):
+            for j in range(3 - body.com_ellipsoid.rank()):
+                V = body.com_ellipsoid.directions()
+                c = body.com_ellipsoid.center()
+                v = V[:, -(j + 1)]
+                constraints.append(v @ (c - coms[i, :]))
+        return np.array(constraints)
+
+    def _solve_one(self, x0, j, method="SLSQP"):
+        """Solve the optimization problem given the initial guess."""
+        constraints = [{"type": "ineq", "fun": self._ineq_constraints, "args": (j,)}]
+        if self.n_eq_con > 0:
+            constraints.append({"type": "eq", "fun": self._eq_constraints})
+        res = minimize(
+            self._cost,
+            x0,
+            method=method,
+            constraints=constraints,
+        )
+        assert res.success, f"Optimization problem failed to solve for index {j}."
+        s_opt, masses_opt, coms_opt = self._parse_args(res.x)
+        return s_opt, masses_opt, coms_opt
+
+    def solve(self, masses0, coms0, method="SLSQP"):
+        """Solve the optimization problem given the initial guess."""
+        x0 = np.concatenate(([0], masses0, coms0.flatten()))
+        radii_squared_opt = np.zeros(3)
+        for j in range(3):
+            s_opt, masses_opt, coms_opt = self._solve_one(x0, j, method=method)
+            radii_squared_opt[j] = s_opt
+        return radii_squared_opt
+
+
 def sample_com(bodies, boundary=True):
     """Sample a single possible center of mass for the composite."""
     n = len(bodies)
@@ -121,7 +210,14 @@ def compose_com_ellipsoid(bodies, N=100, eps=0.01):
 
 
 def compose_radii_of_gyration(bodies):
-    return bodies[0].radii_of_gyration
+    """Compute upper bound on possible radii of gyration."""
+    # Here we assume radii are along Cartesian directions
+    masses_guess = np.array([0.5 * (body.mass_min + body.mass_max) for body in bodies])
+    coms_guess = np.array([body.com_ellipsoid.center() for body in bodies])
+
+    problem = BoundingRadiiOfGyrationProblem(bodies)
+    radii_squared_opt = problem.solve(masses_guess, coms_guess)
+    return np.sqrt(radii_squared_opt)
 
 
 def compose_bounded_bodies(bodies):
