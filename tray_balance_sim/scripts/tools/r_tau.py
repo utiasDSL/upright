af81aed0e95ee0e38e1119b37ea382d8a0efdd31
Adam Heins <mail@adamheins.com>
2022-03-21 22:56:55 -0400

    Implement r_tau computation for equilateral triangle.

---
+++ tray_balance_sim/scripts/tools/r_tau.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import numpy as np
+
+from tray_balance_sim import geometry
+
+import IPython
+
+
+def main():
+    s = 0.2
+    h = geometry.equilateral_triangle_inscribed_radius(s)
+    r = 2 * h
+
+    r_tau_eqtri = geometry.equilateral_triangle_r_tau(s)
+    r_tau_in = geometry.circle_r_tau(h)
+    r_tau_out = geometry.circle_r_tau(r)
+
+    print(f"triangle = {r_tau_eqtri}")
+    print(f"in circ  = {r_tau_in}")
+    print(f"out circ = {r_tau_out}")
+
+
+if __name__ == "__main__":
+    main()
--- tray_balance_sim/src/tray_balance_sim/geometry.py
+++ tray_balance_sim/src/tray_balance_sim/geometry.py
@@ -21,3 +21,18 @@ def rectangle_r_tau(w, h):
     """r_tau for a rectangular support area with uniform friction."""
     # see pushing notes
     return (alpha_rect(w, h) + alpha_rect(h, w)) / (w * h)
+
+
+def equilateral_triangle_area(side_length):
+    """Area of an equilateral triangle."""
+    return np.sqrt(3) * side_length**2 / 4
+
+
+def equilateral_triangle_r_tau(side_length):
+    """r_tau for equilateral triangle."""
+    h = equilateral_triangle_inscribed_radius(side_length)
+    θ = np.pi / 3.0
+    sec = 1. / np.cos(θ)
+    tan = np.tan(θ)
+    area = equilateral_triangle_area(side_length)
+    return h**3 * (tan * sec + np.log(tan + sec)) / area
