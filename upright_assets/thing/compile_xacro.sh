034549c50e049fe1bab01db281b5e390d2623fe4
Adam Heins <mail@adamheins.com>
2022-06-21 13:30:33 -0400

    Load UR10 arm from external xacro packages.

--- upright_assets/thing/compile_xacro.sh
+++ upright_assets/thing/compile_xacro.sh
@@ -10,5 +10,9 @@ xacro xacro/ocs2/mm_ocs2_fingers.urdf.xacro -o urdf/mm_ocs2_fingers.urdf
 xacro xacro/pyb/mm_pyb_tray.urdf.xacro -o urdf/mm_pyb_tray.urdf
 xacro xacro/pyb/mm_pyb_fingers.urdf.xacro -o urdf/mm_pyb_fingers.urdf
 xacro xacro/obstacles.urdf.xacro -o urdf/obstacles.urdf
+
+# second run to resolve mesh paths
+xacro urdf/mm_pyb_tray.urdf -o urdf/mm_pyb_tray.urdf
+
 # xacro mm_pyb_static_obs.urdf.xacro -o mm_pyb_static_obs.urdf
 # xacro mm_static_tray_pyb.urdf.xacro -o mm_static_tray_pyb.urdf
