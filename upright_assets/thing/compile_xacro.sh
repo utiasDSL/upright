6556146e6aff7d7cd4b72adf180f75d9a12478af
Adam Heins <mail@adamheins.com>
2022-06-23 17:04:45 -0400

    Add Ridgeback wheels to a URDF for ROS.
    We need all actuated joints to be put into the ROS parameter server
    under the /robot_description.

--- upright_assets/thing/compile_xacro.sh
+++ upright_assets/thing/compile_xacro.sh
@@ -3,7 +3,7 @@
 # libraries. This is mainly used to resolve ROS package paths without
 # hardcoding.
 mkdir -p urdf
-# xacro mm_ocs2.urdf.xacro -o mm_ocs2.urdf
+
 xacro xacro/ocs2/mm_ocs2_tray.urdf.xacro -o urdf/mm_ocs2_tray.urdf
 xacro xacro/ocs2/mm_ocs2_fingers.urdf.xacro -o urdf/mm_ocs2_fingers.urdf
 
@@ -11,11 +11,14 @@ xacro xacro/pyb/mm_pyb_tray.urdf.xacro -o urdf/mm_pyb_tray.urdf
 xacro xacro/pyb/mm_pyb_fingers.urdf.xacro -o urdf/mm_pyb_fingers.urdf
 xacro xacro/obstacles.urdf.xacro -o urdf/obstacles.urdf
 
+xacro xacro/ros/mm_ros_tray.urdf.xacro -o urdf/mm_ros_tray.urdf
+
 # second run to resolve mesh paths
 xacro urdf/mm_pyb_tray.urdf -o urdf/mm_pyb_tray.urdf
 xacro urdf/mm_ocs2_tray.urdf -o urdf/mm_ocs2_tray.urdf
 xacro urdf/mm_pyb_fingers.urdf -o urdf/mm_pyb_fingers.urdf
 xacro urdf/mm_ocs2_fingers.urdf -o urdf/mm_ocs2_fingers.urdf
+xacro urdf/mm_ros_tray.urdf -o urdf/mm_ros_tray.urdf
 
 # xacro mm_pyb_static_obs.urdf.xacro -o mm_pyb_static_obs.urdf
 # xacro mm_static_tray_pyb.urdf.xacro -o mm_static_tray_pyb.urdf
