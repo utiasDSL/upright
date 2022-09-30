9d793e1470d53a79c0d4000346727d9f709e0f22
Adam Heins <mail@adamheins.com>
2022-09-30 16:02:11 -0400

    Numerical issues with the full optimization problem.

--- upright_cmd/scripts/tools/pyb_contact_points.py
+++ upright_cmd/scripts/tools/pyb_contact_points.py
@@ -144,6 +144,7 @@ def nearest_distance(o1, o2):
 
 def solve_lp(o1, o2, c, x0):
     C = np.concatenate((c, -c))
+
     def cost(x):
         return C @ x
 
@@ -167,55 +168,146 @@ def solve_lp(o1, o2, c, x0):
     return res.x.reshape((2, 3))
 
 
-
-def two_object_opt(o1, o2):
-    d, p, n = nearest_distance(o1, o2)
-    if d > 1e-6:
-        print("not in contact")
-        return [], n
-
+def solve_full_lp(o1, o2, n, x0):
     x = np.array([1, 0, 0])
     y = np.array([0, 1, 0])
     c = np.cross(n, x)
     if np.linalg.norm(c) <= 1e-3:
         c = np.cross(n, y)
 
-    # first find eight candidate vertices (they are all points on the boundary)
-    # TODO we could solve this is a single optimization, since all the C are
-    # computed beforehand
     c1 = c / np.linalg.norm(c)
-    X1 = solve_lp(o1, o2, c1, p)
-
     c2 = np.cross(n, c1)
-    X2 = solve_lp(o1, o2, c2, p)
-
     c3 = (c1 + c2) / np.linalg.norm(c1 + c2)
-    X3 = solve_lp(o1, o2, c3, p)
-
     c4 = np.cross(n, c3)
-    X4 = solve_lp(o1, o2, c4, p)
 
-    X = np.vstack((X1, X2, X3, X4))
+    C = np.concatenate((c1, -c1, c2, -c2, c3, -c3, c4, -c4))
+    X0 = np.tile(x0, 8)
+
+    def cost(X):
+        return C @ X
+
+    def jac(X):
+        return C
+
+    def ineq_con(X):
+        X = X.reshape((8, 3))
+        dists = []
+        for i in range(X.shape[0]):
+            d1 = point_object_distance(X[i, :], o1)[0]
+            d2 = point_object_distance(X[i, :], o2)[0]
+            dists.append(d1)
+            dists.append(d2)
+        return -np.array(dists) + 1e-6
+
+    res = minimize(
+        cost,
+        jac=jac,
+        x0=X0,
+        method="slsqp",
+        constraints=[{"type": "ineq", "fun": ineq_con}],
+    )
+    IPython.embed()
+    return res.x.reshape((8, 3))
+
+
+def two_object_opt(o1, o2, tol=1e-3):
+    d, p, n = nearest_distance(o1, o2)
+    if d > 1e-6:
+        print("not in contact")
+        return [], n
+
+    # x = np.array([1, 0, 0])
+    # y = np.array([0, 1, 0])
+    # c = np.cross(n, x)
+    # if np.linalg.norm(c) <= tol:
+    #     c = np.cross(n, y)
+    #
+    # # first find eight candidate vertices (they are all points on the boundary)
+    # # TODO we could solve this is a single optimization, since all the C are
+    # # computed beforehand
+    # c1 = c / np.linalg.norm(c)
+    # X1 = solve_lp(o1, o2, c1, p)
+    #
+    # c2 = np.cross(n, c1)
+    # X2 = solve_lp(o1, o2, c2, p)
+    #
+    # c3 = (c1 + c2) / np.linalg.norm(c1 + c2)
+    # X3 = solve_lp(o1, o2, c3, p)
+    #
+    # c4 = np.cross(n, c3)
+    # X4 = solve_lp(o1, o2, c4, p)
+    #
+    # X0 = np.vstack((X1, X2, X3, X4))
+
+    X = solve_full_lp(o1, o2, n, p)
+
+    # remove points that are too close together
+    D = squareform(pdist(X))
+    unique = 1 - np.sum(np.tril(D < 1e-3, k=-1), axis=1)
+    unique_idx = np.nonzero(unique)[0]
+
+    # only the unique points
+    X = X[unique_idx, :]
+    D = D[unique_idx, :][:, unique_idx]
+
+    # if there is only one or two (necessarily unique) points left, they are
+    # the vertices
+    if X.shape[0] <= 2:
+        return X, n
+
+    # the two points farthest apart are definitely vertices
+    idx = np.unravel_index(np.argmax(D), D.shape)
+
+    # the other one or two vertices are the remaining points that maximize and
+    # minimize, respectively, the distance to the line connecting the two
+    # current vertices
+    x0 = X[idx[0], :]
+    x1 = X[idx[1], :]
+    a = np.cross(n, x1 - x0)
+    a = a / np.linalg.norm(a)
+
+    proj = (X - x0) @ a
+    i1 = np.argmax(proj)
+    i2 = np.argmin(proj)
+
+    # if either distance is too small, we don't include the vertex because it
+    # is just on the line between the two existing ones
+    indices = list(idx)
+    if np.abs(proj[i1]) > tol:
+        indices.append(i1)
+    if np.abs(proj[i2]) > tol:
+        indices.append(i2)
+
+    IPython.embed()
+
+    return X[indices, :], n
+
 
     # the two points farthest apart are definitely vertices (unless they are
     # the same point)
-    D = squareform(pdist(X))
     idx = np.unravel_index(np.argmax(D), D.shape)
+
+    # TODO we can actually reason this out by index. If min(idx) >= 4:
+    # assert idx != (3, 4), "unexpected index!"
+    # if np.min(idx) >= 4:
+    #     return X[4:, :], n
+    # else:
+    #     return X[:4, :], n
+
     max_dist = D[idx]
 
-    tol = 1e-3
-    if max_dist < tol:
-        indices = [idx[0]]
-    else:
-        indices = list(idx)
-        ds = np.sum(D[:, idx], axis=1)
-        other_idx = np.argpartition(ds, 2)[-2:]
+    # find as many as two other non-colinear points for the other vertices
+    # TODO is it possible point 4 could be co-linear with 1-3 or 2-3?
+    indices = list(idx)
+    ds = np.sum(D[:, idx], axis=1)
+    other_idx = np.argpartition(ds, 2)[-2:]
 
-        for i in range(2):
-            if ds[other_idx[i]] > max_dist + tol:
-                indices.append(other_idx[i])
+    for i in range(2):
+        if ds[other_idx[i]] > max_dist + tol:
+            indices.append(other_idx[i])
 
     vertices = X[indices, :]
+    IPython.embed()
 
     return vertices, n
 
@@ -249,23 +341,24 @@ def main():
     )
     pyb.configureDebugVisualizer(pyb.COV_ENABLE_GUI, 0)
 
-    # o1 = box([0.2, 0.2, 0.2], [0, 0, 0.1], color=(1, 0, 0, 0.5))
-    # o2 = box([0.2, 0.2, 0.2], [0.2, 0.1, 0.1], color=(0, 0, 1, 0.5))
-    # o3 = cylinder(0.1, 0.2, [0, 0.2, 0.1], color=(0, 1, 0, 0.5))
-    #
+    o1 = box([0.2, 0.2, 0.2], [0, 0, 0.1], color=(1, 0, 0, 0.5))
+    o2 = box([0.2, 0.2, 0.2], [0.2, 0.1, 0.1], color=(0, 0, 1, 0.5))
+    o3 = cylinder(0.1, 0.2, [0, 0.2, 0.1], color=(0, 1, 0, 0.5))
+
     # vs1, n = two_object_opt(o1, o2)
+    vs2, _ = two_object_opt(o1, o3)
+    # vs3, _ = two_object_opt(o2, o3)
+
+    # o1 = box([0.2, 0.2, 0.2], [0, 0, 0.1], color=(1, 0, 0, 0.5))
+    # o2 = box([0.2, 0.2, 0.2], [0.3, 0.1, 0.1], color=(0, 0, 1, 0.5))
+    # o3 = box([0.4, 0.2, 0.1], [0.2, 0, 0.25], color=(0, 1, 0, 0.5))
+    #
+    # # vs1, _ = two_object_opt(o1, o2)
     # vs2, _ = two_object_opt(o1, o3)
     # vs3, _ = two_object_opt(o2, o3)
 
-    o1 = box([0.2, 0.2, 0.2], [0, 0, 0.1], color=(1, 0, 0, 0.5))
-    o2 = box([0.2, 0.2, 0.2], [0.3, 0.1, 0.1], color=(0, 0, 1, 0.5))
-    o3 = box([0.4, 0.2, 0.1], [0.2, 0, 0.25], color=(0, 1, 0, 0.5))
-
-    vs1, _ = two_object_opt(o1, o2)
-    vs2, _ = two_object_opt(o1, o3)
-    vs3, _ = two_object_opt(o2, o3)
-
-    vs = np.vstack((vs2, vs3))
+    # vs = np.vstack((vs1, vs2, vs3))
+    vs = vs2
 
     colors = [[0, 0, 0] for _ in vs]
     pyb.addUserDebugPoints(vs, colors, pointSize=10)
