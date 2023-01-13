537488899ee009c84fed47f3cfae6c059d03f7b9
Adam Heins <mail@adamheins.com>
2023-01-13 14:18:30 -0500

    Reasonably good savgol filtering for acceleration data.

--- upright_cmd/scripts/tools/plot_bag_end_effector_states.py
+++ upright_cmd/scripts/tools/plot_bag_end_effector_states.py
@@ -8,7 +8,6 @@ import argparse
 import numpy as np
 import rosbag
 import matplotlib.pyplot as plt
-from scipy import fft, signal
 
 from mobile_manipulation_central import ros_utils
 import upright_core as core
@@ -16,121 +15,11 @@ import upright_control as ctrl
 import upright_cmd as cmd
 from upright_ros_interface.parsing import parse_mpc_observation_msgs
 
-import IPython
-
 
 def parse_control_model(config_path):
     config = core.parsing.load_config(config_path)
     ctrl_config = config["controller"]
     return ctrl.manager.ControllerModel.from_config(ctrl_config)
-
-
-def parse_states_from_vicon_data(bag):
-    # TODO we would also like to compute the velocity and accelerations from
-    # Vicon data directly
-    tray_msgs = [
-        msg for _, msg, _ in bag.read_messages("/vicon/ThingWoodTray/ThingWoodTray")
-    ]
-    ts, tray_poses = ros_utils.parse_transform_stamped_msgs(
-        tray_msgs, normalize_time=True
-    )
-    n = len(ts)
-    dts = ts[1:] - ts[:-1]
-
-    positions = tray_poses[:, :3]
-
-    # NOTE: we can align the measurements if desired
-    dt = np.mean(dts)
-    # new_ts = dt * np.arange(n)
-    # positions = np.array(ros_utils.interpolate_list(new_ts, ts, positions))
-    # ts = new_ts
-
-    velocities = signal.savgol_filter(positions, 10, 2, deriv=1, delta=dt, axis=0)
-    accelerations = signal.savgol_filter(positions, 30, 2, deriv=2, delta=dt, axis=0)
-
-    # velocities = np.zeros_like(positions)
-    # accelerations = np.zeros_like(positions)
-    # for i in range(1, n - 1):
-    #     velocities[i, :] = (positions[i + 1, :] - positions[i - 1, :]) / (
-    #         ts[i + 1] - ts[i - 1]
-    #     )
-    #
-    # for i in range(2, n - 2):
-    #     accelerations[i, :] = (velocities[i + 1, :] - velocities[i - 1, :]) / (
-    #         ts[i + 1] - ts[i - 1]
-    #     )
-    #
-    # smoothed_velocities = np.zeros_like(velocities)
-    # for i in range(2, n - 2):
-    #     smoothed_velocities[i, :] = np.mean(velocities[i-1:i+2, :], axis=0)
-    # velocities = smoothed_velocities
-    #
-    # w = 12
-    # smoothed_accelerations = np.zeros_like(accelerations)
-    # for i in range(w + 2, n - w - 2):
-    #     smoothed_accelerations[i, :] = np.mean(accelerations[i-w:i+w+1, :], axis=0)
-    # accelerations = smoothed_accelerations
-
-    plt.figure()
-    plt.plot(ts, positions[:, 0], label="x")
-    plt.plot(ts, positions[:, 1], label="y")
-    plt.plot(ts, positions[:, 2], label="z")
-    plt.title("EE position")
-    plt.xlabel("Time [s]")
-    plt.ylabel("Position [m]")
-    plt.legend()
-    plt.grid()
-
-    plt.figure()
-    plt.plot(ts, velocities[:, 0], label="x")
-    plt.plot(ts, velocities[:, 1], label="y")
-    plt.plot(ts, velocities[:, 2], label="z")
-    plt.title("EE velocity")
-    plt.xlabel("Time [s]")
-    plt.ylabel("Velocity [m/s]")
-    plt.legend()
-    plt.grid()
-
-    plt.figure()
-    plt.plot(ts, accelerations[:, 0], label="x")
-    plt.plot(ts, accelerations[:, 1], label="y")
-    plt.plot(ts, accelerations[:, 2], label="z")
-    plt.title("EE acceleration")
-    plt.xlabel("Time [s]")
-    plt.ylabel("Acceleration [m/s]")
-    plt.legend()
-    plt.grid()
-
-    # F = fft.fft(accelerations, axis=0)
-    # ω = fft.fftfreq(n, dts[0])[:n//2]
-    # plt.figure()
-    # plt.plot(ω, 2.0/n * np.abs(F[0:n//2]))
-    # plt.grid()
-    #
-    # for i in range(len(ω)):
-    #     if ω[i] >= 6:
-    #         idx = i
-    #         break
-    # idx = 100
-    #
-    # F[idx:n//2+1, :] = 0
-    # F[n//2+idx:, :] = 0
-    # y = fft.ifft(F, axis=0)
-    # y = np.real(y)
-    #
-    # plt.figure()
-    # plt.plot(ts, y[:, 0], label="x")
-    # plt.plot(ts, y[:, 1], label="y")
-    # plt.plot(ts, y[:, 2], label="z")
-    # plt.title("EE acceleration")
-    # plt.xlabel("Time [s]")
-    # plt.ylabel("Acceleration [m/s]")
-    # plt.legend()
-    # plt.grid()
-    # plt.show()
-
-
-    # IPython.embed()
 
 
 def main():
@@ -142,10 +31,6 @@ def main():
     robot = model.robot
 
     bag = rosbag.Bag(bag_path)
-
-    parse_states_from_vicon_data(bag)
-    # plt.show()
-    # return
 
     mpc_obs_msgs = [
         msg for _, msg, _ in bag.read_messages("/mobile_manipulator_mpc_observation")
---
+++ upright_cmd/scripts/tools/plot_bag_vicon_states.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python3
+"""Plot end effector position, velocity, and acceleration from a bag file.
+
+Also computes the maximum velocity and acceleration.
+"""
+import argparse
+
+import numpy as np
+import rosbag
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
+from scipy import signal
+import seaborn
+
+from mobile_manipulation_central import ros_utils
+import upright_core as core
+
+import IPython
+
+
+VICON_OBJECT_NAME = "ThingWoodTray"
+
+
+def savgol(x, y, window_length, polyorder, deriv=0):
+    """Savgol filter for non-uniformly spaced data.
+
+    Much slower than scipy.signal.savgol_filter because we need to refit the
+    polynomial to every window of data.
+    """
+    assert len(x) == len(y)
+    assert window_length > polyorder
+    assert type(deriv) == int and deriv >= 0
+
+    r = window_length // 2
+    n = len(x)
+    degree = polyorder - deriv
+
+    y_smooth = np.zeros_like(y)
+    for i in range(n):
+        low = max(i - r, 0)
+        high = min(i + r + 1, n)
+        x_window = x[low:high]
+        y_window = y[low:high]
+        poly = np.polynomial.Polynomial.fit(x_window, y_window, polyorder)
+        for _ in range(deriv):
+            poly = poly.deriv()
+        y_smooth[i] = poly(x[i])
+    return y_smooth
+
+
+class FilterUpdater:
+    """Add sliders to a plot to control SavGol filter window size and poly order."""
+    def __init__(
+        self, fig, lines, positions, window_size, polyorder, delta=1.0, deriv=0
+    ):
+        self.fig = fig
+        self.lines = lines
+        self.positions = positions
+        self.window_size = window_size
+        self.polyorder = polyorder
+        self.delta = delta
+        self.deriv = deriv
+
+        ax_window_slider = fig.add_axes([0.35, 0.15, 0.5, 0.03])
+        ax_poly_slider = fig.add_axes([0.35, 0.1, 0.5, 0.03])
+
+        self.window_slider = Slider(
+            ax_window_slider, "Window Size", 5, 100, valinit=window_size, valstep=1
+        )
+        self.poly_slider = Slider(
+            ax_poly_slider, "Poly Order", 1, 10, valinit=polyorder, valstep=1
+        )
+
+        self.window_slider.on_changed(self.update_window_size)
+        self.poly_slider.on_changed(self.update_polyorder)
+
+    def update(self):
+        y = signal.savgol_filter(
+            self.positions,
+            self.window_size,
+            self.polyorder,
+            deriv=self.deriv,
+            delta=self.delta,
+            axis=0,
+        )
+        for i, line in enumerate(self.lines):
+            line.set_ydata(y[:, i])
+        self.fig.canvas.draw_idle()
+
+    def update_window_size(self, window_size):
+        self.window_size = window_size
+        self.update()
+
+    def update_polyorder(self, polyorder):
+        self.polyorder = polyorder
+        self.update()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("bagfile", help="Bag file to plot.")
+    args = parser.parse_args()
+
+    bag = rosbag.Bag(args.bagfile)
+
+    topic = ros_utils.vicon_topic_name(VICON_OBJECT_NAME)
+    msgs = [msg for _, msg, _ in bag.read_messages(topic)]
+    ts, poses = ros_utils.parse_transform_stamped_msgs(msgs, normalize_time=True)
+
+    n = len(ts)
+    positions = poses[:, :3]
+    dt = np.mean(ts[1:] - ts[:-1])  # average time step
+
+    # smoothed velocities and accelerations using Savitzky-Golay filter
+    window_size = 31
+    polyorder = 2
+    smooth_velocities = signal.savgol_filter(
+        positions, window_size, polyorder, deriv=1, delta=dt, axis=0
+    )
+    smooth_accelerations = signal.savgol_filter(
+        positions, window_size, polyorder, deriv=2, delta=dt, axis=0
+    )
+
+    # (noisy) velocities and accelerations computed by finite differences
+    velocities = np.zeros_like(positions)
+    accelerations = np.zeros_like(positions)
+    for i in range(1, n - 1):
+        velocities[i, :] = (positions[i + 1, :] - positions[i - 1, :]) / (
+            ts[i + 1] - ts[i - 1]
+        )
+
+    for i in range(2, n - 2):
+        accelerations[i, :] = (velocities[i + 1, :] - velocities[i - 1, :]) / (
+            ts[i + 1] - ts[i - 1]
+        )
+
+    palette = seaborn.color_palette("deep")
+
+    plt.figure()
+    plt.plot(ts, positions[:, 0], label="x")
+    plt.plot(ts, positions[:, 1], label="y")
+    plt.plot(ts, positions[:, 2], label="z")
+    plt.title("EE position")
+    plt.xlabel("Time [s]")
+    plt.ylabel("Position [m]")
+    plt.legend()
+    plt.grid()
+
+    vel_fig = plt.figure()
+    plt.plot(ts, velocities[:, 0], color=palette[0], alpha=0.2)
+    plt.plot(ts, velocities[:, 1], color=palette[1], alpha=0.2)
+    plt.plot(ts, velocities[:, 2], color=palette[2], alpha=0.2)
+    (l1,) = plt.plot(ts, smooth_velocities[:, 0], label="x", color=palette[0])
+    (l2,) = plt.plot(ts, smooth_velocities[:, 1], label="y", color=palette[1])
+    (l3,) = plt.plot(ts, smooth_velocities[:, 2], label="z", color=palette[2])
+    plt.title("EE velocity")
+    plt.xlabel("Time [s]")
+    plt.ylabel("Velocity [m/s]")
+    plt.legend()
+    plt.grid()
+
+    vel_updater = FilterUpdater(
+        vel_fig, [l1, l2, l3], positions, window_size, polyorder, delta=dt, deriv=1
+    )
+
+    acc_fig = plt.figure()
+    plt.plot(ts, accelerations[:, 0], color=palette[0], alpha=0.2)
+    plt.plot(ts, accelerations[:, 1], color=palette[1], alpha=0.2)
+    plt.plot(ts, accelerations[:, 2], color=palette[2], alpha=0.2)
+    (l1,) = plt.plot(ts, smooth_accelerations[:, 0], label="x", color=palette[0])
+    (l2,) = plt.plot(ts, smooth_accelerations[:, 1], label="y", color=palette[1])
+    (l3,) = plt.plot(ts, smooth_accelerations[:, 2], label="z", color=palette[2])
+    plt.title("EE acceleration")
+    plt.xlabel("Time [s]")
+    plt.ylabel("Acceleration [m/s]")
+    plt.legend()
+    plt.grid()
+
+    acc_updater = FilterUpdater(
+        acc_fig, [l1, l2, l3], positions, window_size, polyorder, delta=dt, deriv=2
+    )
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
