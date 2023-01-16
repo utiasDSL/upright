c7baa819bb932cc8a96054323a3943121e4d0862
Adam Heins <mail@adamheins.com>
2023-01-16 13:11:15 -0500

    Clean up ROS sim slightly.

--- upright_cmd/scripts/simulations/mpc_sim_ros.py
+++ upright_cmd/scripts/simulations/mpc_sim_ros.py
@@ -5,12 +5,11 @@ import datetime
 
 import rospy
 import numpy as np
-from pyb_utils.ghost import GhostSphere
 from pyb_utils.frame import debug_frame_world
 from std_msgs.msg import Empty
 
-from upright_sim import simulation
 from upright_core.logging import DataLogger, DataPlotter
+import upright_sim as sim
 import upright_core as core
 import upright_control as ctrl
 import upright_cmd as cmd
@@ -22,10 +21,6 @@ from mobile_manipulation_central import (
 )
 
 import IPython
-
-
-# TODO make a CLI arg
-USE_REAL_VICON = False
 
 
 def main():
@@ -41,40 +36,39 @@ def main():
 
     # start the simulation
     timestamp = datetime.datetime.now()
-    sim = simulation.BulletSimulation(
-        config=sim_config, timestamp=timestamp, cli_args=cli_args
+    env = sim.simulation.BulletSimulation(
+        config=sim_config, timestamp=timestamp, video_name=cli_args.video
     )
 
     # settle sim to make sure everything is touching comfortably
-    sim.settle(5.0)
+    env.settle(5.0)
 
     # initial time, state, input
     t = 0.0
-    q, v = sim.robot.joint_states()
-    a = np.zeros(sim.robot.nv)
-    x_obs = sim.dynamic_obstacle_state()
+    q, v = env.robot.joint_states()
+    a = np.zeros(env.robot.nv)
+    x_obs = env.dynamic_obstacle_state()
     x = np.concatenate((q, v, a, x_obs))
-    u = np.zeros(sim.robot.nu)
+    u = np.zeros(env.robot.nu)
 
     Q_obs = np.array([0, 0, 0, 1])
 
     # we simulate a Vicon end point for the projectile if (1) we are using a
     # projectile at all and (2) we are not using the real Vicon system
-    use_projectile_interface = len(sim.dynamic_obstacles) > 0 and not USE_REAL_VICON
+    use_projectile_interface = len(env.dynamic_obstacles) > 0
 
     # data logging
     logger = DataLogger(config)
 
-    logger.add("sim_timestep", sim.timestep)
-    logger.add("duration", sim.duration)
-    logger.add("object_names", [str(name) for name in sim.objects.keys()])
+    logger.add("sim_timestep", env.timestep)
+    logger.add("duration", env.duration)
+    logger.add("object_names", [str(name) for name in env.objects.keys()])
 
     logger.add("nq", ctrl_config["robot"]["dims"]["q"])
     logger.add("nv", ctrl_config["robot"]["dims"]["v"])
     logger.add("nx", ctrl_config["robot"]["dims"]["x"])
     logger.add("nu", ctrl_config["robot"]["dims"]["u"])
 
-    # model = ctrl.manager.ControllerModel.from_config(ctrl_config, x0=x)
     model = ctrl.manager.ControllerModel.from_config(ctrl_config)
 
     # reference pose trajectory
@@ -99,7 +93,9 @@ def main():
     ros_interface.publish_time(t)
 
     # publisher for reset command to estimator
-    reset_projectile_pub = rospy.Publisher("reset_projectile_estimate", Empty, queue_size=1)
+    reset_projectile_pub = rospy.Publisher(
+        "reset_projectile_estimate", Empty, queue_size=1
+    )
 
     # wait until a command has been received
     # note that we use real time here since this sim directly controls sim time
@@ -107,8 +103,8 @@ def main():
     while not ros_interface.ready():
         ros_interface.publish_feedback(t, q, v)
         ros_interface.publish_time(t)
-        t += sim.timestep
-        time.sleep(sim.timestep)
+        t += env.timestep
+        time.sleep(env.timestep)
         if rospy.is_shutdown():
             return
 
@@ -116,35 +112,37 @@ def main():
     t0 = t
 
     # add dynamic obstacles and start them moving
-    sim.launch_dynamic_obstacles(t0=t0)
+    env.launch_dynamic_obstacles(t0=t0)
 
     # simulation loop
-    while not rospy.is_shutdown() and t - t0 <= sim.duration:
+    while not rospy.is_shutdown() and t - t0 <= env.duration:
         # feedback is in the world frame
         v_prev = v
-        q, v = sim.robot.joint_states(add_noise=True, bodyframe=False)
+        q, v = env.robot.joint_states(add_noise=True, bodyframe=False)
         ros_interface.publish_feedback(t, q, v)
 
         # publish feedback on dynamic obstacles if using
         if use_projectile_interface:
-            x_obs = sim.dynamic_obstacle_state()
+            x_obs = env.dynamic_obstacle_state()
             r_obs = x_obs[:3]
             projectile_ros_interface.publish_pose(t, r_obs, Q_obs)
             projectile_ros_interface.publish_ground_truth(t, r_obs, x_obs[3:6])
 
         # commands are always in the body frame (to match the real robot)
-        cmd_vel_world = sim.robot.command_velocity(ros_interface.cmd_vel, bodyframe=True)
+        cmd_vel_world = env.robot.command_velocity(
+            ros_interface.cmd_vel, bodyframe=True
+        )
 
         if logger.ready(t):
             # NOTE: we can try to approximate acceleration using finite
             # differences, but it is extremely inaccurate
-            # a = (v - v_prev) / sim.timestep
+            # a = (v - v_prev) / env.timestep
             x = np.concatenate((q, v, a, x_obs))
 
             # log sim stuff
-            r_ew_w, Q_we = sim.robot.link_pose()
-            v_ew_w, ω_ew_w = sim.robot.link_velocity()
-            r_ow_ws, Q_wos = sim.object_poses()
+            r_ew_w, Q_we = env.robot.link_pose()
+            v_ew_w, ω_ew_w = env.robot.link_velocity()
+            r_ow_ws, Q_wos = env.object_poses()
             logger.append("ts", t - t0)
             logger.append("xs", x)
             logger.append("r_ew_ws", r_ew_w)
@@ -160,7 +158,7 @@ def main():
             logger.append("r_ew_w_ds", r_ew_w_d)
             logger.append("Q_we_ds", Q_we_d)
 
-        t, obs_reset = sim.step(t, step_robot=False)
+        t, obs_reset = env.step(t, step_robot=False)
 
         # if obstacle is reset (i.e. instanstantly reset to a new state), we
         # have to reset the estimator
@@ -178,8 +176,8 @@ def main():
     if cli_args.log is not None:
         logger.save(timestamp, name=cli_args.log)
 
-    if sim.video_manager.save:
-        print(f"Saved video to {sim.video_manager.path}")
+    if env.video_manager.save:
+        print(f"Saved video to {env.video_manager.path}")
 
     # visualize data
     DataPlotter.from_logger(logger).plot_all(show=True)
