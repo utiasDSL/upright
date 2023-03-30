Fix CWC sign error; add new faster QP formulation.

upright_robust/scripts/constraint_elimination.py
@@ -45,7 +45,8 @@ def extract_z_np(Λ):
 def compute_Q_matrix(f):
     D = rob.body_regressor_by_vector_acceleration_matrix(f)
     Dg = -D[:3, :]
-    Z = rob.body_regressor_by_vector_velocity_matrix_half(f)
+    # Z = rob.body_regressor_by_vector_velocity_matrix_half(f)
+    Z = rob.body_regressor_by_vector_velocity_matrix(f)
 
     nv = 6
     ng = 3
@@ -63,7 +64,8 @@ def compute_Q_matrix(f):
 def solve_global_relaxed(obj, F, idx, other_constr_idx):
     f = F[idx, :]
     Q = compute_Q_matrix(f)
-    Z = rob.body_regressor_by_vector_velocity_matrix_half(f)
+    # Z = rob.body_regressor_by_vector_velocity_matrix_half(f)
+    Z = rob.body_regressor_by_vector_velocity_matrix(f)
 
     nv = 6
     ng = 3
@@ -83,11 +85,14 @@ def solve_global_relaxed(obj, F, idx, other_constr_idx):
     V_min = -V_max
     Λ_max = np.outer(V_max, V_max)
 
+    v_max = 0.5
+    ω_max = 0.5
+
     A = cp.Variable(nv)
     G = cp.Variable(ng)
     z = cp.Variable(nz)
     θ = cp.Variable(nθ)
-    # V = cp.Variable(nv)
+    V = cp.Variable(nv)
 
     y = cp.hstack((A, G, z, θ))
     X = cp.Variable((ny, ny), PSD=True)
@@ -103,21 +108,21 @@ def solve_global_relaxed(obj, F, idx, other_constr_idx):
     # notice that we don't need to include V in the optimization problem at all
     objective = cp.Maximize(0.5 * cp.trace(Q @ X))
     constraints = [
-        # z == cp.vec(Λ),
+        z == cp.vec(Λ),
         # z == vech(Λ),
-        z == extract_z(Λ),
+        # z == extract_z(Λ),
 
         # we constrain z completely through Λ
         # Λ <= Λ_max,
-        cp.trace(Λ[:3, :3]) <= 0.5**2,
-        cp.trace(Λ[3:, 3:]) <= 0.5**2,
+        cp.trace(Λ[:3, :3]) == v_max**2,  # NOTE this constraint is tight
+        cp.trace(Λ[3:, 3:]) <= ω_max**2,
 
-        # schur(Λ, V) >> 0,
+        schur(Λ, V) >> 0,
         # cp.diag(Λ) <= np.diag(Λ_max),
         # constraints on X
         schur(X, y) >> 0,
-        cp.trace(Xa[:3, :3]) <= 1,
-        cp.trace(Xa[3:, 3:]) <= 1,
+        cp.trace(Xa[:3, :3]) == 1,
+        cp.trace(Xa[3:, 3:]) == 1,
         # cp.diag(Xa) <= np.maximum(A_max**2, A_min**2),
         cp.diag(Xθ) <= np.maximum(obj.θ_max**2, obj.θ_min**2),
         # Xa <= np.maximum(np.outer(A_max,A_max), np.outer(A_min, A_min)),
@@ -129,13 +134,16 @@ def solve_global_relaxed(obj, F, idx, other_constr_idx):
         # Xz <= vech_np(Λ_max) @ vech(Λ).T,
         # Xz <= extract_z_np(Λ_max) @ extract_z(Λ).T,
         # Xv == Λ,
-        # cp.trace(Xz) <= 2 * cp.trace(Λ),
+        cp.trace(Xz) == (v_max**2 + ω_max**2) * cp.trace(Λ),
+
+        # X[nv + ng : nv + ng + nz, :] == 0,
+        # X[:, nv + ng : nv + ng + nz] == 0,
 
         # TODO can we make this tighter by depending directly on Λ?
         # note that this is still conservative regardless because we are not
         # reasoning directly about the norm inequalities we have
 
-        cp.diag(Xz) <= extract_z_np(Λ_max)**2,
+        # cp.diag(Xz) <= extract_z_np(Λ_max)**2,
         # cp.trace(Xz) <= np.linalg.norm(extract_z_np(Λ_max))**2,
         # cp.trace(Xz) <= np.linalg.norm(extract_z_np(Λ_max)) * cp.norm1(extract_z(Λ)),
         # cp.trace(Xz) <= extract_z_np(Λ_max) @ extract_z(Λ),
@@ -150,6 +158,8 @@ def solve_global_relaxed(obj, F, idx, other_constr_idx):
         cp.norm(A[3:]) <= 1,
         θ >= obj.θ_min,
         θ <= obj.θ_max,
+        cp.norm(V[:3]) <= v_max,
+        cp.norm(V[3:]) <= ω_max,
         # V >= V_min,
         # V <= V_max,
     ]
@@ -161,16 +171,19 @@ def solve_global_relaxed(obj, F, idx, other_constr_idx):
 
     # off-diagonal blocks of z @ z.T are symmetric, which helps tighten the
     # relaxation
-    # for i in range(0, 5):
-    #     for j in range(i + 1, 5):
-    #         Xz_ij = Xz[i * 6 : (i + 1) * 6, j * 6 : (j + 1) * 6]
-    #         constraints.append(Xz_ij == Xz_ij.T)
-    #         constraints.append(Xz_ij << V_max[i] * V_max[j] * Λ)
-    #         constraints.append(Xz_ij >> V_max[i] * V_min[j] * Λ)
-    #
-    # for i in range(6):
-    #     Xz_ii = Xz[i * 6 : (i + 1) * 6, i * 6 : (i + 1) * 6]
-    #     constraints.append(Xz_ii << V_max[i] ** 2 * Λ)
+    for i in range(0, 5):
+        for j in range(i + 1, 5):
+            Xz_ij = Xz[i * 6 : (i + 1) * 6, j * 6 : (j + 1) * 6]
+
+            constraints.append(Xz_ij == Xz_ij.T)
+            constraints.append(cp.trace(Xz_ij[:3, :3]) == Λ[i, j] * v_max**2)
+            constraints.append(cp.trace(Xz_ij[3:, 3:]) == Λ[i, j] * ω_max**2)
+            # constraints.append(Xz_ij >> V_max[i] * V_min[j] * Λ)
+
+    for i in range(6):
+        Xz_ii = Xz[i * 6 : (i + 1) * 6, i * 6 : (i + 1) * 6]
+        constraints.append(cp.trace(Xz_ii[:3, :3]) == Λ[i, i] * v_max**2)
+        constraints.append(cp.trace(Xz_ii[3:, 3:]) == Λ[i, i] * ω_max**2)
 
     problem = cp.Problem(objective, constraints)
     # problem.solve(solver=cp.MOSEK, verbose=True)
@@ -179,24 +192,10 @@ def solve_global_relaxed(obj, F, idx, other_constr_idx):
     except cp.error.SolverError:
         print("failed to solve relaxed problem")
     print(problem.status)
-    print(problem.value)
     # print(np.linalg.eigvals(X.value))
 
-    # TODO this is an undervalue of the true objective that contains V directly: f @ Y(C, V, A) @ θ
-    # print(0.5 * y.value @ Q @ y.value)
-
-    # extract the relevant value of V
-    # e, v = np.linalg.eig(Λ.value)
-    # if np.abs(e[0]) > 1e-8:
-    #     V = np.sqrt(e[0]) * v[:, 0]
-    #     V = V / np.max(np.abs(V)) * V_max[0]
-    # else:
-    #     V = np.zeros(6)
-    # Ag = np.concatenate((G.value, np.zeros(3)))
-    # Y = rob.body_regressor(V, A.value + Ag)
-    # print(f @ Y @ θ.value)
-
-    # IPython.embed()
+    IPython.embed()
+    return problem.value
 
 
 # def solve_local(obj, f):
@@ -205,7 +204,8 @@ def solve_local(obj, F, idx, other_constr_idx):
     # A, g, V, θ
     A0 = np.zeros(6)
     G0 = np.array([0, 0, -9.81])
-    V0 = np.zeros(6)
+    # V0 = np.array([0.5, 0, 0, 0.5, 0, 0])
+    V0 = np.array([0, 0.5, 0, 0, 0.5, 0])
     θ0 = obj.θ
     x0 = np.concatenate((A0, G0, V0, θ0))
 
@@ -218,6 +218,9 @@ def solve_local(obj, F, idx, other_constr_idx):
     A_max = np.ones(6)
     V_min = -np.ones(6)
     V_max = np.ones(6)
+
+    v_max = 0
+    ω_max = 0
 
     f = F[idx, :]
 
@@ -249,7 +252,7 @@ def solve_local(obj, F, idx, other_constr_idx):
                 # V_max - V,
                 # V - V_min,
                 [1 - np.linalg.norm(A[:3]), 1 - np.linalg.norm(A[3:])],
-                [0.5 - np.linalg.norm(V[:3]), 0.5 - np.linalg.norm(V[3:])],
+                [v_max - np.linalg.norm(V[:3]), ω_max - np.linalg.norm(V[3:])],
                 obj.θ_max - θ,
                 θ - obj.θ_min,
                 [-g * np.cos(max_tilt_angle) - G[2]],
@@ -272,10 +275,13 @@ def solve_local(obj, F, idx, other_constr_idx):
     value = -cost(res.x)
     print(f"local solved = {res.success}")
     print(f"local obj = {value}")
+    print(f"||v|| = {np.linalg.norm(V[:3])}, ||ω|| = {np.linalg.norm(V[3:])}")
     if value <= 0:
         print("^^^^ NEGATIVE")
     if not res.success:
         IPython.embed()
+    # IPython.embed()
+    return value
 
 
 def main():
@@ -295,11 +301,13 @@ def main():
 
     for i in range(F.shape[0]):
         print(i)
-        solve_global_relaxed(obj, F, i, list(range(i)))
-        solve_local(obj, F, i, list(range(i)))
+        # solve_global_relaxed(obj, F, i, list(range(i)))
+        # solve_local(obj, F, i, list(range(i)))
 
-        # solve_global_relaxed(obj, F, i, [])
-        # solve_local(obj, F, i, [])
+        relaxed = solve_global_relaxed(obj, F, i, [])
+        local = solve_local(obj, F, i, [])
+        print(f"gap = {relaxed - local}")
+        return
 
 
 if __name__ == "__main__":

upright_robust/scripts/optimize_acceleration.py
@@ -100,7 +100,7 @@ def optimize_acceleration_face_form(C, V, ad, obj, a_bound=10, α_bound=10):
     def ineq_con(x):
         A = x
         giw = rob.body_gravito_inertial_wrench(C, V, A, obj)
-        return F @ giw
+        return F @ giw  # NOTE this is positive
 
     res = minimize(
         cost,
@@ -118,7 +118,7 @@ def optimize_acceleration_face_form(C, V, ad, obj, a_bound=10, α_bound=10):
 def optimize_acceleration_robust(C, V, ad, obj, a_bound=10, α_bound=10):
     """Optimize acceleration to best matched desired subject to balancing constraints."""
     contacts = obj.contacts()
-    F = -rob.cwc(contacts)
+    F = rob.cwc(contacts)
     Ag = np.concatenate((C @ G, np.zeros(3)))
 
     # first 6 decision variables are the acceleration, then a ton of duals
@@ -196,10 +196,73 @@ def optimize_acceleration_robust(C, V, ad, obj, a_bound=10, α_bound=10):
     print(f"solve took {t1 - t0} seconds")
     A = x[:6]
 
-    R = np.linalg.pinv(J_eq)
-    N = scipy.linalg.null_space(J_eq)
+    return A
 
-    IPython.embed()
+def optimize_acceleration_robust_face(C, V, ad, obj, a_bound=10, α_bound=10):
+    """Optimize acceleration to best matched desired subject to balancing constraints."""
+    contacts = obj.contacts()
+    F = rob.cwc(contacts)
+    Ag = np.concatenate((C @ G, np.zeros(3)))
+
+    # first 6 decision variables are the acceleration, then a ton of duals
+    # {λ_i}
+    nf = F.shape[0]
+    nv = 6
+    x0 = np.zeros(nv)
+
+    # initial guess
+    x0[:3] = ad
+
+    # fmt: off
+    P_tilde = np.block([
+        [obj.P, obj.p[:, None]],
+        [np.zeros((1, obj.P.shape[1])), np.array([[-1]])]])
+    # fmt: on
+    R = rob.span_to_face_form(P_tilde.T)[0]
+
+    # pre-compute Jacobians
+    n_ineq = R.shape[0]  # dimension of one set of equality constraints
+    N_ineq = n_ineq * nf  # dim of all equality constraints
+    J_ineq = np.zeros((N_ineq, nv))
+    d_ineq = np.zeros(N_ineq)
+    for i in range(nf):
+        d, D = rob.body_regressor_by_vector_matrix(C, V, F[i, :])
+        d_tilde = np.append(d, 0)
+        D_tilde = np.vstack((D, np.zeros((1, D.shape[1]))))
+        d_ineq[i * n_ineq : (i + 1) * n_ineq] = R @ d_tilde
+
+        # Jacobian w.r.t. A
+        J_ineq[i * n_ineq : (i + 1) * n_ineq, :] = R @ D_tilde
+
+    P = np.zeros((nv, nv))
+    P[:3, :3] = np.eye(3)
+    P[3:6, 3:6] = 0.01 * np.eye(3)
+    q = np.zeros(nv)
+    q[:3] = -ad
+
+    ub = np.concatenate((a_bound * np.ones(3), α_bound * np.ones(3)))
+    lb = -ub
+
+    # NOTE that we don't actually *need* a high-accuracy solution: any feasible
+    # solution is acceptable. Warm-starting also will likely help a lot.
+    t0 = time.time()
+    x = solve_qp(
+        P=sparse.csc_matrix(P),
+        q=q,
+        G=sparse.csc_matrix(-J_ineq),
+        h=d_ineq,
+        lb=lb,
+        ub=ub,
+        initvals=x0,
+        eps_abs=1e-6,
+        eps_rel=1e-6,
+        max_iter=10000,
+        solver="osqp",
+        # polish=True,
+    )
+    t1 = time.time()
+    print(f"solve took {t1 - t0} seconds")
+    A = x[:6]
 
     return A
 
@@ -207,7 +270,7 @@ def optimize_acceleration_robust(C, V, ad, obj, a_bound=10, α_bound=10):
 def optimize_acceleration_robust_osqp(C, V, ad, obj, a_bound=5, α_bound=1):
     """Optimize acceleration to best matched desired subject to balancing constraints."""
     contacts = obj.contacts()
-    F = -rob.cwc(contacts)
+    F = rob.cwc(contacts)
     Ag = np.concatenate((C @ G, np.zeros(3)))
 
     # first 6 decision variables are the acceleration, then a ton of duals
@@ -309,7 +372,7 @@ def inner_optimization(C, V, A, obj):
     # the max one
 
     contacts = obj.contacts()
-    F = -rob.cwc(contacts)
+    F = rob.cwc(contacts)
     Ag = np.concatenate((C @ G, np.zeros(3)))  # body frame gravity
 
     # first opt var is y, the rest are the inertial parameters θ
@@ -392,7 +455,7 @@ def main():
     obj = rob.BalancedObject(m=1, h=0.1, δ=0.05, μ=0.2, h0=0, x0=0)
 
     # test the body regressor
-    # Ag = np.concatenate((C @ G, np.zeros(3)))  # body frame gravity
+    Ag = np.concatenate((C @ G, np.zeros(3)))  # body frame gravity
     # A = np.array([1, 2, 3, 4, 5, 6])
     # Y = body_regressor(C, V, A - Ag)
     # Y0, Ys = body_regressor_components(C, V)
@@ -408,7 +471,9 @@ def main():
     # L = rob.lift6_matrices()
     # IPython.embed()
 
-    # rob.cwc(obj.contacts())
+    # contacts = obj.contacts()
+    # F = rob.cwc(obj.contacts())
+    # giw = rob.body_gravito_inertial_wrench(C, V, np.zeros(6), obj)
     # IPython.embed()
     # return
 
@@ -416,8 +481,10 @@ def main():
     # A1 = optimize_acceleration(C, V, ad_body, obj)
     # ys, ds, λs = inner_optimization(C, V, A1, obj)
     Ar1 = optimize_acceleration_robust(C, V, ad_body, obj)
+    Ar2 = optimize_acceleration_robust_face(C, V, ad_body, obj)
     # Ar2 = optimize_acceleration_sequential(C, V, ad_body, obj)
     print(Ar1)
+    print(Ar2)
 
 
 if __name__ == "__main__":

upright_robust/src/upright_robust/__init__.py
@@ -84,11 +84,16 @@ class ContactPoint:
 
         # span (generator) form matrix FC = {Sz | z >= 0}
         # fmt: off
-        self.S = P @ np.array([
-            [1,  1, 1,  1],
-            [μ, -μ, 0,  0],
-            [0,  0, μ, -μ]
-        ])
+        # self.S = P @ np.array([
+        #     [1,  1, 1,  1],
+        #     [μ, -μ, 0,  0],
+        #     [0,  0, μ, -μ]
+        # ])
+        self.S = np.vstack([
+            self.normal + μ * self.span[0, :],
+            self.normal + μ * self.span[1, :],
+            self.normal - μ * self.span[0, :],
+            self.normal - μ * self.span[1, :]]).T
         # fmt: on
 
 
@@ -144,6 +149,7 @@ def body_gravito_inertial_wrench(C, V, A, obj):
     The supplied velocity twist V and acceleration A must also be in the body
     frame.
     """
+    G = np.array([0, 0, -9.81])  # TODO
     Ag = np.concatenate((C @ G, np.zeros(3)))
     return obj.M @ (A - Ag) + skew6(V) @ obj.M @ V
 
@@ -225,7 +231,9 @@ def span_to_face_form(S):
     """Convert the span form of a polyhedral cone to face form.
 
     Span form is { Sz | z  >= 0 }
-    Face form is { x  | Fx >= 0 }
+    Face form is { x  | Ax <= b }
+
+    Return (A, b) of the face form.
     """
     # span form
     # we have generators as columns but cdd wants it as rows, hence the transpose
@@ -235,7 +243,7 @@ def span_to_face_form(S):
     # polyhedron
     poly = cdd.Polyhedron(Smat)
 
-    # face form: Ax <= b
+    # general face form is Ax <= b, which cdd stores as one matrix [b -A]
     Fmat = poly.get_inequalities()
     F = np.array([Fmat[i] for i in range(Fmat.row_size)])
     b = F[:, 0]
@@ -253,7 +261,7 @@ def cwc(contacts):
     A, b = span_to_face_form(S)
     assert np.allclose(b, 0)
 
-    # Fw >= 0 ==> there exist feasible contact forces to support wrench w
+    # Aw <= 0 implies there exist feasible contact forces to support wrench w
     return A
 
 