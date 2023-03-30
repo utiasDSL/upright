503b2ea4ad34a86e95d21c05abe9cc9df038c3c1
Adam Heins <mail@adamheins.com>
2023-03-30 16:20:21 -0400

    Implement dual global constraint elimination problem.

--- upright_robust/scripts/constraint_elimination.py
+++ upright_robust/scripts/constraint_elimination.py
@@ -198,14 +198,80 @@ def solve_global_relaxed(obj, F, idx, other_constr_idx):
     return problem.value
 
 
-# def solve_local(obj, f):
-def solve_local(obj, F, idx, other_constr_idx):
+def solve_global_relaxed_dual(obj, F, idx, other_constr_idx, v_max=1, ω_max=1):
+    f = F[idx, :]
+
+    Z = rob.body_regressor_by_vector_velocity_matrix(f)
+    D = rob.body_regressor_by_vector_acceleration_matrix(f)
+    Dg = -D[:3, :]
+
+    nv = 6
+    ng = 3
+    nz = Z.shape[0]
+
+    # fmt: off
+    P_tilde = np.block([
+        [obj.P, obj.p[:, None]],
+        [np.zeros((1, obj.P.shape[1])), np.array([[-1]])]])
+    # fmt: on
+    R = rob.span_to_face_form(P_tilde.T)[0]
+    R = R / np.max(np.abs(R))
+
+    # gravity constraints
+    z_normal = np.array([0, 0, 1])
+    max_tilt_angle = np.deg2rad(30)
+
+    # velocity and acceleration constraints
+    a_max = 1
+    α_max = 1
+
+    A = cp.Variable(nv)
+    G = cp.Variable(ng)
+    z = cp.Variable(nz)
+    Λ = cp.Variable((6, 6), PSD=True)
+
+    values = []
+    Vs = []
+
+    for i in range(R.shape[0]):
+        objective = cp.Minimize(R[i, :-1] @ (Z.T @ z + Dg.T @ G + D.T @ A))
+        constraints = [
+            z == cp.vec(Λ),
+
+            # we constrain z completely through Λ
+            cp.trace(Λ[:3, :3]) <= v_max**2,
+            cp.trace(Λ[3:, 3:]) <= ω_max**2,
+
+            # gravity constraints
+            cp.norm(G) <= g,
+            z_normal @ G <= -g * np.cos(max_tilt_angle),
+
+            # acceleration constraints
+            cp.norm(A[:3]) <= 1,
+            cp.norm(A[3:]) <= 1,
+        ]
+        problem = cp.Problem(objective, constraints)
+        t0 = time.time()
+        problem.solve(solver=cp.MOSEK)
+        t1 = time.time()
+        # print(f"Δt = {t1 - t0}")
+        values.append(problem.value)
+
+        e, v = np.linalg.eig(Λ.value)
+        Vs.append(np.sqrt(e[0]) * v[:, 0])
+
+    min_idx = np.argmin(values)
+    print(f"relaxed value = {values[min_idx]}")
+    print(f"relaxed V = {Vs[min_idx]}")
+    return np.min(values)
+
+
+def solve_local(obj, F, idx, other_constr_idx, v_max=1, ω_max=1):
 
     # A, g, V, θ
     A0 = np.zeros(6)
     G0 = np.array([0, 0, -9.81])
-    # V0 = np.array([0.5, 0, 0, 0.5, 0, 0])
-    V0 = np.array([0, 0.5, 0, 0, 0.5, 0])
+    V0 = np.array([v_max, 0, 0, ω_max, 0, 0])
     θ0 = obj.θ
     x0 = np.concatenate((A0, G0, V0, θ0))
 
@@ -218,9 +284,6 @@ def solve_local(obj, F, idx, other_constr_idx):
     A_max = np.ones(6)
     V_min = -np.ones(6)
     V_max = np.ones(6)
-
-    v_max = 0
-    ω_max = 0
 
     f = F[idx, :]
 
@@ -275,7 +338,8 @@ def solve_local(obj, F, idx, other_constr_idx):
     value = -cost(res.x)
     print(f"local solved = {res.success}")
     print(f"local obj = {value}")
-    print(f"||v|| = {np.linalg.norm(V[:3])}, ||ω|| = {np.linalg.norm(V[3:])}")
+    # print(f"||v|| = {np.linalg.norm(V[:3])}, ||ω|| = {np.linalg.norm(V[3:])}")
+    print(f"local V = {V}")
     if value <= 0:
         print("^^^^ NEGATIVE")
     if not res.success:
@@ -283,31 +347,106 @@ def solve_local(obj, F, idx, other_constr_idx):
     # IPython.embed()
     return value
 
+def solve_local_dual(obj, F, idx, other_constr_idx, v_max=1, ω_max=1):
+
+    # A, g, V, θ
+    A0 = np.zeros(6)
+    G0 = np.array([0, 0, -9.81])
+    V0 = np.array([v_max, 0, 0, ω_max, 0, 0])
+
+    # limits
+    z_normal = np.array([0, 0, 1])
+    max_tilt_angle = np.deg2rad(30)
+
+    # fmt: off
+    P_tilde = np.block([
+        [obj.P, obj.p[:, None]],
+        [np.zeros((1, obj.P.shape[1])), np.array([[-1]])]])
+    # fmt: on
+    R = rob.span_to_face_form(P_tilde.T)[0]
+    R = R / np.max(np.abs(R))
+
+    a_max = 1
+    α_max = 1
+
+    f = F[idx, :]
+
+    values = []
+    Vs = []
+
+    for i in range(R.shape[0]):
+        x0 = np.concatenate((A0, G0, V0))
+
+        def cost(x):
+            A, G, V = x[:6], x[6:9], x[9:15]
+            Ag = np.concatenate((G, np.zeros(3)))
+
+            # NOTE bit of a hack by settings C == 0
+            d, D = rob.body_regressor_by_vector_matrix(np.zeros((3, 3)), V, f)
+
+            return R[i, :-1] @ (d + D @ (A - Ag))
+
+        def eq_cons(x):
+            G = x[6:9]
+            # return np.linalg.norm(G) - g
+            return G @ G - g**2
+
+        def ineq_cons(x):
+            A, G, V = x[:6], x[6:9], x[9:15]
+            # squared formulation is numerically better for the solver (it is
+            # differentiable)
+            return np.concatenate(
+                (
+                    [a_max**2 - np.linalg.norm(A[:3])**2, α_max**2 - np.linalg.norm(A[3:])**2],
+                    [v_max**2 - np.linalg.norm(V[:3])**2, ω_max**2 - np.linalg.norm(V[3:])**2],
+                    [-g * np.cos(max_tilt_angle) - G[2]],
+                )
+            )
+
+        res = minimize(
+            cost,
+            x0=x0,
+            method="slsqp",
+            constraints=[
+                {"type": "ineq", "fun": ineq_cons},
+                {"type": "eq", "fun": eq_cons},
+            ],
+        )
+        A, G, V = res.x[:6], res.x[6:9], res.x[9:15]
+        value = cost(res.x)
+        if not res.success:
+            print("failed to solve local problem!")
+            IPython.embed()
+        # else:
+        #     print("success")
+        values.append(value)
+        Vs.append(V)
+
+    min_idx = np.argmin(values)
+    print(f"local value = {values[min_idx]}")
+    print(f"local V = {Vs[min_idx]}")
+    return np.min(values)
+
 
 def main():
     np.set_printoptions(precision=5, suppress=True)
 
     obj = rob.BalancedObject(m=1, h=0.1, δ=0.05, μ=0.5, h0=0, x0=0)
-    F = -rob.cwc(obj.contacts())
-
+    F = rob.cwc(obj.contacts())
     F = F / np.max(np.abs(F))
-
-    # solve_local(obj, F[13, :])
-    # solve_global_relaxed(obj, F[13, :])
-    # return
-
-    # solve_global_relaxed(obj, F, 2, [0, 1])
-    # return
 
     for i in range(F.shape[0]):
         print(i)
         # solve_global_relaxed(obj, F, i, list(range(i)))
         # solve_local(obj, F, i, list(range(i)))
 
-        relaxed = solve_global_relaxed(obj, F, i, [])
-        local = solve_local(obj, F, i, [])
-        print(f"gap = {relaxed - local}")
-        return
+        # relaxed = solve_global_relaxed(obj, F, i, [])
+        relaxed = solve_global_relaxed_dual(obj, F, i, [])
+        local = solve_local_dual(obj, F, i, [])
+        if local >= 0:
+            print("CONSTRAINT NEVER ACTIVE!")
+        print(f"gap = {relaxed - local}\n")
+        # return
 
 
 if __name__ == "__main__":
